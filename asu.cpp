836eee6dc65b07154826748bb92e52c4d19238ff
AlejandroPapi <natmetallica@outlook.es>
2016-09-29 20:08:37 -0500

    Papacito :*

---
+++ asu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string papi;
+	cout << "Introduce tu nombre papacito :*" << endl;
+	cin >> papi;
+	if (papi == "Alejandro"){
+		cout << "Eres un papacito amf ;D" << endl; 
+		cout << "Te la chupo y me la chupas osea 69 <3" << endl;
+	} else {
+		cout << "Eres un marica" << endl;
+	}
+	return 0;
+}
+
